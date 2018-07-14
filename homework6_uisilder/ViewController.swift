4839127db2c0064be9f68ce073499e0d5f39090f
黃冠霖 <mac@macs-macbook-pro.local>
2018-07-14 21:47:39 +0800

    Initial Commit

---
+++ homework6_uisilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework6_uisilder
+//
+//  Created by mac on 2018/7/14.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
